9d293f4bf497d909d4d91cb68fab66680de57d9d
JizongL <2015rpro@gmail.com>
2019-05-12 11:23:42 -0400

    updated readme

---
+++ build/precache-manifest.1dc940a048baac34c4f335c6c11d7295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0499a4273c24506cde6",
+    "url": "/static/css/main.e2881b66.chunk.css"
+  },
+  {
+    "revision": "d0499a4273c24506cde6",
+    "url": "/static/js/main.169db1f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "394f230fabad0ead4bc9",
+    "url": "/static/js/2.9e404467.chunk.js"
+  },
+  {
+    "revision": "9905c68e5572c61bfe9af2c6d9a3ddb7",
+    "url": "/index.html"
+  }
+];
